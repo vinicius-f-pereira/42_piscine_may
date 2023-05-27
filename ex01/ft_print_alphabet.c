70bdd674fe9f062b1213e0ad0747f2c2ca1c86b3
Vinicius de Freitas Pereira <vde-frei@student.42sp.org.br>
2023-05-27 13:29:36 -0300

    put print

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/27 12:16:06 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/05/27 13:27:51 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		ft_putchar(alphabet);
+		alphabet++;
+	}
+}
