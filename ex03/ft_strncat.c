2d37348c2934b6d55e4ad785317acae4a5a00d6c
Vinicius de Freitas Pereira <vde-frei@student.42sp.org.br>
2023-06-11 04:33:04 -0300

    ncat

---
+++ ex03/ft_strncat.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/11 04:15:16 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/06/11 04:31:15 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while(*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int	i;
+	int	dest_len;
+
+	i = 0;
+	dest_len = ft_strlen(dest);
+	while(src[i] != '\0' && nb)
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
+	dest[dest_len + 1] = '\0';
+	return (dest);
+}
+//#include <stdio.h>
+//int	main (void)
+//{
+//	char a[] = "hello";
+//	char b[] = "worldas";
+//
+//	ft_strncat(a, b, 100);
+//	printf("%s", a);
+//	return (0);
+//}
