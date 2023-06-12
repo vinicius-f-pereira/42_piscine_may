4f3c5b641c8bcd34559296ab512671492ecfb9c5
Vinicius de Freitas Pereira <vde-frei@student.42sp.org.br>
2023-06-12 17:33:49 -0300

    fix

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 05:20:06 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/06/12 07:32:57 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/06/12 17:33:09 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,26 @@ int	ft_strlen(char *str)
 	}
 	return (len);
 }
-unsigned int    ft_strlcat(char *dest, char *src, unsigned int size)
-{
-    unsigned int    dest_len;
-    unsigned int    src_len;
-    unsigned int    i;
 
-    dest_len = ft_strlen(dest);
-    src_len = ft_strlen(src);
-    i = 0;
-    while (src[i] && dest_len + i + 1 < size)
-    {
-        dest[dest_len + i] = src[i];
-        i++;
-    }
-    dest[dest_len + i] = '\0';
-    if (size < dest_len)
-        return (src_len + size);
-    else
-        return (src_len + dest_len);
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	tsrc_len;
+	unsigned int	i;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	i = 0;
+	while (src[i] && dest_len + i + 1 < size)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	if (size < dest_len)
+		return (src_len + size);
+	else
+		return (src_len + dest_len);
 }
 //#include <stdio.h>
 //int    main(void)
