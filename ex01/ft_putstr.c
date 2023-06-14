3c3848df82018bcf7d9c286e5e8fc5785b7dfd61
Vinicius de Freitas Pereira <vde-frei@student.42sp.org.br>
2023-06-14 05:13:29 -0300

    run to the hills

---
+++ ex01/ft_putstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 21:50:44 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/06/13 22:01:02 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+//int main(void)
+//{
+//	char *a = "Segue firme, vamos passar!";
+//
+//	ft_putstr(a);
+//	return (0);
+//}
